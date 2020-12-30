0b7e18a238795c17c6dc58ac4fec3a8e0c56681a
Andy Pearson <andy@londonparkour.com>
2020-12-30 15:58:27 +0000

    absolute

--- modules/andyp/manifests/zsh_profile.pp
+++ modules/andyp/manifests/zsh_profile.pp
@@ -4,16 +4,16 @@ class andyp::zsh_profile {
   # Version Control System REPO - Download Repo
   #
   # Add my ZSH repo
-  vcsrepo { '~/':
+  vcsrepo { '/root/':
     ensure   => present,
     provider => git,
     source   => 'https://github.com/IORoot/zsh.git',
   }
 
   # Symlink
-  file { '~/.zshrc':
+  file { '/root/.zshrc':
     ensure => 'link',
-    target => '~/zsh/.vagrant.zshrc',
+    target => '/root/zsh/.vagrant.zshrc',
   }
   
 }
