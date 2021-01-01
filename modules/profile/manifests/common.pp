926397af2d51a2677d32e9483a938baccb2a2927
Andy Pearson <andy@londonparkour.com>
2021-01-01 14:54:34 +0000

    Update oh-my-zsh

--- modules/profile/manifests/common.pp
+++ modules/profile/manifests/common.pp
@@ -7,11 +7,21 @@ class profile::common {
   include firewall
 
   # Install ZSH
-  class { 'zsh':
-    package => true,
-  }
+  class { 'zsh': package => true, }
 
-  # Install my ZSH git repo
-  include andyp::zsh_profile
+  $directory        = '/root/.oh-my-zsh'
+  $directory_exists = find_file($directory)
+
+  if $directory_exists { } else {
+
+    # Install oh-my-zsh
+    exec { 'install_oh_my_zsh':
+      command => '/usr/bin/sh -c "$(/usr/bin/wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+    }
+
+    # Install my ZSH git repo
+    include andyp::zsh_profile
+
+  }
 
 }
