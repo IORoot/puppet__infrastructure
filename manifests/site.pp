a30fdc63b1de570e9c22618e25bba4919570cdf1
Andy Pearson <andy@londonparkour.com>
2021-01-01 13:42:59 +0000

    Add dev.pulse

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,3 +5,8 @@ node 'dev.labs.londonparkour.com' {
   include role::webserver
   include role::vagrant
 }
+
+node 'dev.pulse.londonparkour.com' {
+  include role::webserver
+  include role::vagrant
+}
