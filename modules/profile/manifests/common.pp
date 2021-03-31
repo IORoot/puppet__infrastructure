cf60ec79dfc99fb2975f790aaf30c602968a7907
Andy Pearson <andy@londonparkour.com>
2021-03-31 08:04:02 +0100

    ohmyzsh class install for root

--- modules/profile/manifests/common.pp
+++ modules/profile/manifests/common.pp
@@ -9,21 +9,12 @@ class profile::common {
   # Install ZSH
   class { 'zsh': package => true, }
 
-  class { 'ohmyzsh': }
+  # Install ohmyzsh
+  ohmyzsh::install { 'root':
+    set_sh => true
+  }
 
   # Install my ZSH git repo
   include andyp::zsh_profile
-
-  # $directory        = '/root/.oh-my-zsh'
-  # $directory_exists = find_file($directory)
-
-  # if $directory_exists { } else {
-
-  #   # Install oh-my-zsh
-  #   exec { 'install_oh_my_zsh':
-  #     command => '/usr/bin/sh -c "$(/usr/bin/wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
-  #   }
-
-  # }
 
 }
