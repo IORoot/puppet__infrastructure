3a9cb018fd436ef48995b68a5eaa72922bb1683f
Andy Pearson <andy@londonparkour.com>
2021-03-31 08:30:58 +0100

    remove

--- modules/profile/manifests/common.pp
+++ modules/profile/manifests/common.pp
@@ -10,8 +10,6 @@ class profile::common {
   class { 'zsh': package => true, }
 
   # oh-myzsh is through YAML
-  ohmyzsh::install { ['root']: set_sh => true }
-
 
   # Install my ZSH setup
   include andyp::zsh_profile
