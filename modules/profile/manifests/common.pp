e4b9e8ff1558ecabb3653c8eb3c040c6fd5edeac
Andy Pearson <andy@londonparkour.com>
2021-03-31 08:25:15 +0100

    plugin

--- modules/profile/manifests/common.pp
+++ modules/profile/manifests/common.pp
@@ -10,6 +10,16 @@ class profile::common {
   class { 'zsh': package => true, }
 
   # oh-myzsh is through YAML
+  ohmyzsh::install { ['root']: set_sh => true }
+  ohmyzsh::plugins { 'root': plugins =>
+    [
+      'zsh-autosuggestions'          => [
+        'ensure'= latest,
+        'source'= git,
+        'url   '= 'https://github.com/zsh-users/zsh-autosuggestions.git',
+      ]
+    ]
+  }
 
   # Install my ZSH setup
   include andyp::zsh_profile
