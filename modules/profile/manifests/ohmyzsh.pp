5a9fd47e6b42e46792628747355f01aa444c023d
Andy Pearson <andy@londonparkour.com>
2021-07-02 11:49:24 +0100

    up

--- modules/profile/manifests/ohmyzsh.pp
+++ modules/profile/manifests/ohmyzsh.pp
@@ -1,8 +1,12 @@
 class profile::ohmyzsh{
 
+  exec { 'whereami':
+    command => '/bin/cd ~; /usr/bin/echo /usr/bin/pwd; /bin/whoami;'
+  }
+
   # Delete default .zshrc - test not a symbolic link (-h)
   exec { 'install_ohmyzsh':
-      command => '/bin/cd ~; /usr/bin/echo /usr/bin/pwd; /usr/bin/sh -c "$(/usr/bin/wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"',
+      command => '/usr/bin/sh -c "$(/usr/bin/wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"',
       onlyif  => '/usr/bin/test ! -d ~/.oh-my-zsh',
   }
 }
