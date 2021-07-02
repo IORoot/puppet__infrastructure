7fcbba8bad6c635cc5f3c63e0f5775acbea698ba
Andy Pearson <andy@londonparkour.com>
2021-07-02 11:59:11 +0100

    only if

--- modules/profile/manifests/ohmyzsh.pp
+++ modules/profile/manifests/ohmyzsh.pp
@@ -1,19 +1,15 @@
 class profile::ohmyzsh{
 
-  exec { 'whereami':
-    command => '/usr/bin/echo /usr/bin/pwd; /bin/whoami > /tmp/info;'
-  }
-
   # Delete default .zshrc - test not a symbolic link (-h)
   exec { 'download_ohmyzsh':
       command => '/usr/bin/wget -O ~/install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)',
-      onlyif  => '/usr/bin/test ! -d ~/.oh-my-zsh',
+      onlyif  => '/usr/bin/test ! -f ~/install.sh',
   }
 
   # Install
   exec { 'install_ohmyzsh':
       command => '/bin/sh ~/install.sh',
-      onlyif  => '/usr/bin/test ! -d ~/.oh-my-zsh',
+      onlyif  => '/usr/bin/test ! -f ~/install.sh',
   }
 
 }
