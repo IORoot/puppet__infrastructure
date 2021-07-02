87eb6c43ad028c344e1855c7e9d4f392b1285fc5
Andy Pearson <andy@londonparkour.com>
2021-07-02 12:18:12 +0100

    plugins

--- modules/profile/manifests/ohmyzsh.pp
+++ modules/profile/manifests/ohmyzsh.pp
@@ -18,4 +18,15 @@ class profile::ohmyzsh{
       onlyif  => '/usr/bin/test ! -d /root/.oh-my-zsh/custom/themes/powerlevel10k/',
   }
 
+  # Install zsh-autosuggestions plugin
+  exec { 'zsh_autosuggestions':
+      command => '/usr/bin/git clone https://github.com/zsh-users/zsh-autosuggestions.git /root/.oh-my-zsh/custom/plugins/zsh-autosuggestions',
+      onlyif  => '/usr/bin/test ! -d /root/.oh-my-zsh/custom/plugins/zsh-autosuggestions/',
+  }
+  # Install zsh-syntax-highlighting plugin
+  exec { 'zsh_syntax_highlighting':
+      command => '/usr/bin/git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /root/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting',
+      onlyif  => '/usr/bin/test ! -d /root/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/',
+  }
+
 }
