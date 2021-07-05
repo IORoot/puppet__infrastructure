2b12bb41adcc22e4a7f8f5a95580937be161af29
Andy Pearson <andy@londonparkour.com>
2021-07-05 09:25:08 +0100

    vagrant first

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,20 +2,20 @@ node 'default' {
 }
 
 node 'dev.londonparkour.com' {
-  include role::webserver
   include role::vagrant
+  include role::webserver
   include role::qa
   include vhost::dev_londonparkour_com
 }
 
 node 'dev.labs.londonparkour.com' {
-  include role::webserver
   include role::vagrant
+  include role::webserver
   include vhost::dev_labs_londonparkour_com
 }
 
 node 'dev.pulse.londonparkour.com' {
-  include role::webserver
   include role::vagrant
+  include role::webserver
   include vhost::dev_pulse_londonparkour_com
 }
